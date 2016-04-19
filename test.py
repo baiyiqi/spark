26fc2fc2f54cf2c7196eecea430ab9aeaf75dec4
byq818 <yiqi.bai@gmail.com>
2016-04-18 22:08:31 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,15 @@
+__author__ = 'yiqibai'
+
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("file:///Users/yiqibai/PycharmProjects/spark/ml-100k/u.data")
+ratings = lines.map(lambda x : x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.iteritems():
+    print "%s, %i" % (key, value)
